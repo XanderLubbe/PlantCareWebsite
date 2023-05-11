95bebc325d3e931e2f58d9e30ca9461923414250
vniej002 <juanr@bbd.co.za>
2023-05-11 09:54:47 +0200

    Improved overall strucutre.
    Removed ejs stuffs. And tweaked the structure.

---
+++ controller/plantController.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const plantAPI = require('../data-access/plantModel');
+
+const rootDir = path.dirname(__dirname);
+
+exports.index = (req, res) => {
+    res.sendFile(rootDir + '/views/index.html');
+}
+
+exports.showList = (req, res) => {
+    plantAPI.getPlantList(req.params.query)
+    .then(responseData => {
+      console.log(responseData.data);
+      res.send(responseData.data);
+    })
+    .catch(error => {
+      console.error('Error retrieving species list:', error);
+      res.status(500).send('An error occurred while retrieving the species list');
+    });
+}
+
+exports.showDetails = (req, res) => {
+    plantAPI.getPlantDetails(req.params.id)
+    .then(responseData => {
+      console.log(responseData);
+      res.send(responseData);
+    })
+    .catch(error => {
+      console.error('Error retrieving species details:', error);
+      res.status(500).send('An error occurred while retrieving the species details');
+    });
+}
