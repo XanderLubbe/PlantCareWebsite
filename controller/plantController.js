9ce593a77687d982da6af371dd1f8c9a808e306c
Xander Lubbe BBD <xanderl@bbd.co.za>
2023-05-15 08:04:44 +0200

    Updates

--- controller/plantController.js
+++ controller/plantController.js
@@ -36,10 +36,10 @@ exports.showDetails = (req, res) => {
 
 exports.testy = async (req, res) => {
   const plantDataArray = getPlantDataStub()
-  const plantInfoTile = await ejs.renderFile(rootDir + '/views/plantTileInfoBubble.ejs')
+  const plantInfoBubbles = await ejs.renderFile(rootDir + '/views/plantTileInfoBubble.ejs', )
   const plantTilePromises = []
   for(let i = 0; i < plantDataArray.length; i++){
-    plantTilePromises.push( ejs.renderFile(rootDir + '/views/plantTiles.ejs', { plantInfoTiles: plantInfoTile, imageUrl: plantDataArray[i].imageUrl } ) )
+    plantTilePromises.push( ejs.renderFile(rootDir + '/views/plantTiles.ejs', { plantInfoBubbles: plantInfoBubbles, imageUrl: plantDataArray[i].imageUrl, nickName: plantDataArray[i].nickName } ) )
   }
   const plantTiles = await Promise.all(plantTilePromises)
   const html = await ejs.renderFile(rootDir + '/views/myPlants.ejs', {plantTiles})
@@ -47,28 +47,37 @@ exports.testy = async (req, res) => {
 }
 
 // stub
+// (plantApiId, plantName, scientificName, otherName, plantImage, plantType, categoryId) values(1, 'European Silver Fir', 'Abies Alba','Common Silver Fir', 'img1.png', 'tree',1) 
+
 function getPlantDataStub() {
   return [
     {
       imageUrl: '/static/images/philodendron.jpg',
+      nickName: 'Leo',
     },
     {
       imageUrl: '/static/images/aloe.jpg',
+      nickName: 'Mike',
     },
     {
       imageUrl: '/static/images/bromeliad.jpg',
+      nickName: 'Juan-Roux',
     },
     {
       imageUrl: '/static/images/orchid.jpg',
+      nickName: 'Zeerak',
     },
     {
       imageUrl: '/static/images/sanserveria.jpg',
+      nickName: 'Johan',
     },
     {
       imageUrl: '/static/images/sedum.jpg',
+      nickName: 'Xander',
     },
     {
       imageUrl: '/static/images/spathiphyllum.jpg',
+      nickName: 'Bobby',
     },
   ]
 }
