336f412dd4b27cf33b91a63badfd90ce4049e739
vniej002 <juanr@bbd.co.za>
2023-05-16 19:01:52 +0200

    Fixed merge conflict

--- controller/userController.js
+++ controller/userController.js
@@ -1,67 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const User = require('../models/user');
+const path = require("path");
+const User = require("../models/user");
 
 const rootDir = path.dirname(__dirname);
 
 exports.getIndex = (req, res) => {
-    req.session.user = {succeeded: false};
-    res.render('Login/login.ejs', {result: ''});
-}
+  req.session.user = { succeeded: false };
+  res.render("Login/login.ejs", { result: "" });
+};
 
 exports.postIndex = (req, res) => {
-    const errorMessage = "Looks like you have been planted at the login screen. Try again!";
+  const errorMessage = "Username or Password is incorrect";
 
-    const user = new User(req.body);
-    user.validateUser(user)
-    .then(responseData => {
-        if (responseData.succeeded === true){
-            req.session.user = responseData;
-            console.log(responseData);
-            res.redirect('/dashboard');
-        }
-        else {
-            res.render('Login/login.ejs', {result: errorMessage});
-        }
+  const user = new User(req.body);
+  user
+    .validateUser(user)
+    .then((responseData) => {
+      if (responseData.succeeded === true) {
+        req.session.user = responseData;
+        res.redirect('/dashboard');
+      } else {
+        res.render("Login/login.ejs", { result: errorMessage });
+      }
     })
-    .catch(error => {
-        console.error('Error registering user:', error);
-        res.render('Login/login.ejs', {result: error});
+    .catch((error) => {
+      console.error("Error registering user:", error);
+      res.render("Login/login.ejs", { result: error });
     });
-}
+};
 
 exports.getRegister = (req, res) => {
-    res.render('Login/register.ejs', {result: ''});
-}
+  res.render("Login/register.ejs", { result: "" });
+};
+
+exports.errors = (req, res) => {
+  res.render("ErrorPages/Construction.ejs", { result: "" });
+};
 
 exports.postRegister = (req, res) => {
-    const responseMessage = 'Successfully registered!';
-    const errorMessage = 'Unferntunately failed registering!';
+  const responseMessage = "Successfully registered!";
+  const errorMessage = "Unferntunately failed registering!";
 
-    const user = new User(req.body);
-    user.registerUser(user)
-    .then(responseData => {
-        if (responseData === true){
-            res.render('Login/login.ejs', {result: responseMessage});
-        } else{
-            res.render('Login/register.ejs', {result: errorMessage});
-        }
-        
+  const user = new User(req.body);
+  user
+    .registerUser(user)
+    .then((responseData) => {
+      if (responseData === true) {
+        res.render("Login/login.ejs", { result: responseMessage });
+      } else {
+        res.render("Login/register.ejs", { result: errorMessage });
+      }
     })
-    .catch(error => {
-        console.error('Error registering user:', error);
-        res.render('Login/register.ejs', {result: error});
+    .catch((error) => {
+      console.error("Error registering user:", error);
+      res.render("Login/register.ejs", { result: error });
     });
-}
+};
 
-exports.dashboard = (req, res) =>{
-    if (!req.session.user || !req.session.user.succeeded) {
-        let errorMessage = "Oops, it looks like your login info didn't grow on us. Please try again.";
-        res.render('Login/login.ejs', {result: errorMessage});
-    }
-    else{
-        console.log(req.session.user.username);
-        res.render('dashboard.ejs', {user: req.session.user});
-    }
+exports.dashboard = (req, res) => {
+  if (!req.session.user || !req.session.user.succeeded) {
+    let errorMessage =
+      "Oops, it looks like your login info didn't grow on us. Please try again.";
+    res.render("Login/login.ejs", { result: errorMessage });
+  } else {
+    res.render("dashboard.ejs", { user: { name: "TBD" } });
+  }
 };
