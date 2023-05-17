dd1a04e4eb15c660b28bd70af34b80fe926bbf55
Xander Lubbe BBD <xanderl@bbd.co.za>
2023-05-17 09:52:14 +0200

    Started dashboard elements

--- controller/indexController.js
+++ controller/indexController.js
@@ -6,6 +6,8 @@ exports.index = (req, res) =>{
     res.sendFile(rootDir + '/views/index.html');
 };
 
-exports.dashboard = (req, res) =>{
-    res.sendFile(rootDir + '/views/dashboard.html');
+exports.dashboard = async (req, res) =>{
+  const html = await ejs.renderFile(rootDir + '/views/dashboard.ejs',{totalPlants})
+  res.status(200).send(html)
+  res.sendFile(rootDir + '/views/dashboard.ejs');
 };
