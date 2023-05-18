cc0136da095d7f7008d63a53743a396b77ab874a
Xander Lubbe BBD <xanderl@bbd.co.za>
2023-05-18 13:18:57 +0200

    Fixed merge conflicts

---
+++ controller/authenticationController.js
@@ -0,0 +1,11 @@
+exports.auth = (req, res, next) => {
+    if (!req.session.user || !req.session.user.succeeded) {
+        let errorMessage =
+          "Oops, it looks like your login info didn't grow on us. Please try again.";
+        req.session.message = errorMessage;
+        res.redirect("/");
+    }
+    else{
+        next();
+    }
+}
