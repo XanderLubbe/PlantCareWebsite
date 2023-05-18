135f0d2cb1101702194475cd0d4d296a37544141
Xander Lubbe BBD <xanderl@bbd.co.za>
2023-05-18 10:16:29 +0200

    Removed unused const

--- controller/plantController.js
+++ controller/plantController.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const plantAPI = require('../data-access/plantModel');
 const ejs = require('ejs');
 const {Plant} = require('../models/plant');
 
