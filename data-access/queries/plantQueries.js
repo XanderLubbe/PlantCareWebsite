c4a908bac5c53a2dcf88065216d5f901aa18b137
Xander Lubbe BBD <xanderl@bbd.co.za>
2023-05-18 14:20:06 +0200

    Started addPlant work

--- data-access/queries/plantQueries.js
+++ data-access/queries/plantQueries.js
@@ -1,8 +1,8 @@
-exports.getPlantByName  = `SELECT plantApiId, plantName, scientificName, otherName, plantImage, plantType FROM plant WHERE plantName LIKE ? OR scientificName LIKE ? OR otherName LIKE ?`;
+exports.getPlantByName  = `SELECT plantId, plantApiId, plantName, scientificName, otherName, plantImage, plantType FROM plant WHERE plantName LIKE ? OR scientificName LIKE ? OR otherName LIKE ?`;
 
-exports.getPlantById  = `SELECT plantApiId, plantName, scientificName, otherName, plantImage, plantType FROM plant WHERE plantId = ?`;
+exports.getPlantById  = `SELECT plantId, plantApiId, plantName, scientificName, otherName, plantImage, plantType FROM plant WHERE plantId = ?`;
 
-exports.getPlants  = `SELECT plantApiId, plantName, scientificName, otherName, plantImage, plantType FROM plant`;
+exports.getPlants  = `SELECT plantId, plantApiId, plantName, scientificName, otherName, plantImage, plantType FROM plant`;
 
 exports.insertPlant = `INSERT INTO plant (plantApiId, plantName, scientificName, otherName, plantImage, plantType, categoryId) VALUES (?, ?, ?, ?, ?, ?, 1)` ;
 
