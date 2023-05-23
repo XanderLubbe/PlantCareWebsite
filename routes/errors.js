aa08c0934b1ccafe136254ae5a3268c54583b1df
vniej002 <juanr@bbd.co.za>
2023-05-23 09:04:52 +0200

    Changed error route.

--- routes/errors.js
+++ routes/errors.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const errorController = require("../controller/errorController");
 
-router.get("*", errorController.errors);
+router.all("*", errorController.errors);
 
 module.exports = router;
