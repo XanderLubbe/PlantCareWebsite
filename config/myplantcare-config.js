e8b3bb15fcb89f647eb5e4b66ee37c148966943a
vniej002 <juanr@bbd.co.za>
2023-05-24 18:07:56 +0200

    Squashed commit of the following:
    commit f492c71611bad3137a44697472fb28d81d86404f
    Author: vniej002 <juanr@bbd.co.za>
    Date:   Wed May 24 10:03:24 2023 +0200
    
        Changed table create.
    
    commit 4ec82e8457fe2523f91df90f82e1e977ff48dbcf
    Author: vniej002 <juanr@bbd.co.za>
    Date:   Tue May 23 17:39:38 2023 +0200
    
        Added API auth.

--- config/myplantcare-config.js
+++ config/myplantcare-config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
+    api_key: process.env.MYPLANTCARE_API_KEY,
     user_url: process.env.MYPLANTCARE_USER,
     plant_url: process.env.MYPLANTCARE_PLANT,
     userplants_url: process.env.MYPLANTCARE_USERPLANT
